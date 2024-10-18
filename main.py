302b5da74d46d00056a087f54224dfa17fd21dce
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-10-18 08:52:32 +0200

    plotting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import zntrack.examples
+import numpy as np
 
 project = zntrack.Project()
 
@@ -6,5 +7,15 @@ with project:
     zntrack.examples.ParamsToMetrics(
         params={"loss": 0.1, "accuracy": 0.9},
     )
+with project.group("plots"):
+    zntrack.examples.WritePlots(
+        x=list(range(10)),
+        y=list(range(10)),
+    )
+    zntrack.examples.WritePlots(
+        x=list(range(10)),
+        y=np.exp(np.linspace(0, 1, 10)).tolist(),
+    )
+
 
 project.repro()
