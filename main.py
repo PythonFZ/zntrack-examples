de82dc7104ac329fef0042e2cb187486438ffd20
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-10-24 11:33:48 +0200

    add public DVC remote and more nodes

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ with project:
     zntrack.examples.ParamsToMetrics(
         params={"loss": 0.1, "accuracy": 0.9},
     )
+
+with project.group("outs"):
+    zntrack.examples.ParamsToOuts(
+        params="Lorem ipsum",
+        name="StringOuts"
+    )
+    zntrack.examples.ParamsToOuts(
+        params=42,
+        name="NumericOuts"
+    )
 with project.group("plots"):
     zntrack.examples.WritePlots(
         x=list(range(10)),
