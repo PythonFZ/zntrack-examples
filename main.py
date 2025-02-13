420915b73d845f4bc171fb29bedffa74c6786511
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-02-13 11:42:36 +0100

    use def instead of dict

--- main.py
+++ main.py
@@ -54,44 +54,49 @@ models = [
     ),
 ]
 
-config = {
-    "moons": {"method": "make_moons", "params": {"noise": 0.3, "random_state": 0}},
-    "circles": {
-        "method": "make_circles",
-        "params": {"noise": 0.2, "factor": 0.5, "random_state": 0},
-    },
-    "linearly-separable": {
-        "method": "linearly_separable",
-        "params": {
+
+def classify(ds):
+    split = TrainTestSplit(x=ds.x, y=ds.y)
+
+    cfs = []
+
+    for model in models:
+        cfs.append(
+            Classifier(
+                model=model,
+                x=ds.x,
+                x_train=split.x_train,
+                y_train=split.y_train,
+                x_test=split.x_test,
+                y_test=split.y_test,
+                name=model.name,
+            )
+        )
+
+    CombineFigures(cfs=cfs)
+
+
+with project.group("moons"):
+    ds = CreateDataset(method="make_moons", params={"noise": 0.3, "random_state": 0})
+    classify(ds)
+
+with project.group("circles"):
+    ds = CreateDataset(
+        method="make_circles", params={"noise": 0.2, "factor": 0.5, "random_state": 0}
+    )
+    classify(ds)
+
+with project.group("linearly-separable"):
+    ds = CreateDataset(
+        method="linearly_separable",
+        params={
             "n_features": 2,
             "n_redundant": 0,
             "n_informative": 2,
             "random_state": 42,
             "n_clusters_per_class": 1,
         },
-    },
-}
-
-for name, params in config.items():
-    with project.group(name):
-        ds = CreateDataset(**params)
-        split = TrainTestSplit(x=ds.x, y=ds.y)
-
-        cfs = []
-
-        for model in models:
-            cfs.append(
-                Classifier(
-                    model=model,
-                    x=ds.x,
-                    x_train=split.x_train,
-                    y_train=split.y_train,
-                    x_test=split.x_test,
-                    y_test=split.y_test,
-                    name=model.name,
-                )
-            )
-
-        CombineFigures(cfs=cfs, always_changed=True)
+    )
+    classify(ds)
 
 project.repro()
